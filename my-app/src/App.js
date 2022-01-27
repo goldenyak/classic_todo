0bd0280bb6614f64d8a148349052fa33963434c9
Egor <goldenyakegor@gmail.com>
2022-01-27 23:33:26 +0300

    add some functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,21 @@ function App() {
     }
 
     const removeTodo = (todoID) => {
-      setTodos(todos.filter( todo => todo.id !== todoID))
+        setTodos(todos.filter(todo => todo.id !== todoID))
+    }
+
+    const toggleTodoComplete = (todoID) => {
+        setTodos(
+            todos.map(todo => {
+                    if (todo.id !== todoID) return todo;
+
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+            )
+        )
     }
 
 
@@ -32,9 +46,14 @@ function App() {
             </label>
             <ul>
                 {todos.map(todo => <li key={todo.id}>
-                    <input type="checkbox" checked={todo.completed}/>
+                    <input type="checkbox" checked={todo.completed} onChange={() => toggleTodoComplete(todo.id)}/>
                     <span>{todo.text}</span>
-                    <button onClick={() => removeTodo(todo.id)} style={{color: 'red', border: 'none', backgroundColor: 'white', cursor: 'pointer', }}>&times;</button>
+                    <button onClick={() => removeTodo(todo.id)} style={{
+                        color: 'red',
+                        border: 'none',
+                        backgroundColor: 'white',
+                        cursor: 'pointer',
+                    }}>&times;</button>
                 </li>)}
             </ul>
         </div>
